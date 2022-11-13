d740a48cf654e1bdf7b1801c53632dae02dd51ac
MangKong-coder <domango1214@gmail.com>
2022-11-13 23:06:58 +0800

    chore: config `unplugin-icons`

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import Icon from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    AutoImport({
+      resolvers: [IconsResolver({
+        prefix: 'Icon',
+        extension: 'jsx'
+      })]
+    }), Icon({
+      compiler: 'solid',
+      autoInstall: true
+    })
+  ],
 });
