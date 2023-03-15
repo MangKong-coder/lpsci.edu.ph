b632bdd3b04c29b6d31fe49246506964369aaaef
MangKong-coder <domango1214@gmail.com>
2023-03-16 00:41:10 +0800

    feat: add props usage

--- src/components/NewsArticleCard.tsx
+++ src/components/NewsArticleCard.tsx
@@ -1,20 +1,40 @@
 import { JSX } from 'solid-js';
 
-export default function NewsArticleCard(): JSX.Element {
+interface NewsArticleCardProps {
+  coverImg: string;
+  hammer?: string;
+  headline: string;
+  lead: string;
+  author: string;
+  publication: string;
+  articleLink: string;
+}
+
+export default function NewsArticleCard(props: NewsArticleCardProps): JSX.Element {
   return (
     <article class="w-9/12">
       <div>
-        <div class="w-full h-96 rounded border-2  border-lpsci-yellow">
+        <div class="w-56 lg:w-full h-64 lg:h-96 rounded border-2  border-lpsci-yellow">
           <img
-            src="https://images.unsplash.com/photo-1672357867195-33fc57085af7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            src={props.coverImg}
             alt=""
             class="w-full h-full object-cover object-center"
           />
         </div>
-        <div class="py-3">
-          <h1 class="text-4xl mb-0.5 hover:underline">Induction of Newly Elected Club Officers</h1>
-          <p class="text-gray-500 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod...</p>
-          <p class="text-sm text-gray-700">John Doe | Ang Paham of Standard Press</p>
+        <div class="py-3 group">
+          <a href={props.articleLink}>
+            {props.hammer ? <h1 class="text-5xl capitalize font-semibold group-hover:underline">{props.hammer}</h1> : null}
+
+            <h2 class="text-4xl mb-0.5 group-hover:underline">{props.headline}</h2>
+          </a>
+          <p class="text-gray-500 mb-3">{props.lead}</p>
+          <p class="text-sm text-gray-700">
+            {props.author}
+            {' '}
+            |
+            {' '}
+            {props.publication}
+          </p>
         </div>
       </div>
     </article>
